a48624403845e7cf2ea644a53d69b4a28f1e32fc
julianocodifica <julianotypes@gmail.com>
2026-06-23 23:27:14 -0400

    Refatoração: suporte a números decimais e proteção contra divisão por zero

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -10,16 +10,16 @@ void main() {
     while (recomeçar.equalsIgnoreCase("S")) {
         System.out.println("Digite o primeiro número:");
         // Validação se o que foi digitado é um int.
-        while (!scan.hasNextInt()) {
-            System.out.println("Erro: Isso não é um número inteiro válido! Tente novamente:");
+        while (!scan.hasNextDouble()) {
+            System.out.println("Erro: Isso não é um número válido! Tente novamente:");
             scan.nextLine();
         }
-        num1 = scan.nextInt();
+        num1 = scan.nextDouble();
         scan.nextLine(); // Limpa o buffer do Enter após o nextInt().
         System.out.println("Qual operação você quer realizar? (+, -, x, /)");
         String operaçao = scan.nextLine();
         // Validação se o que foi digitado é exatamente uma das operações possíveis. Se não for, volta desde o começo.
-        if(!operaçao.matches("[+-x/]")) {
+        if(!operaçao.matches("[+x/-]")) {
             System.out.println("Operação inválida. Tentando novamente.");
             continue;
         }
@@ -27,48 +27,54 @@ void main() {
             case "+":
                 System.out.println("Digite o número pelo qual você quer somar o primeiro:");
                 // Validação se é um int.
-                while (!scan.hasNextInt()) {
-                    System.out.println("Erro: Isso não é um número inteiro válido! Tente novamente:");
+                while (!scan.hasNextDouble()) {
+                    System.out.println("Erro: Isso não é um número válido! Tente novamente:");
                     scan.nextLine();
                 }
-                num2 = scan.nextInt();
+                num2 = scan.nextDouble();
                 scan.nextLine();
                 result = num1 + num2;
                 System.out.println("O resultado da soma é: " + result);
                 break;
             case "-":
                 System.out.println("Digite o número pelo qual você quer subtrair do primeiro:");
-                while (!scan.hasNextInt()) {
-                    System.out.println("Erro: Isso não é um número inteiro válido! Tente novamente:");
+                while (!scan.hasNextDouble()) {
+                    System.out.println("Erro: Isso não é um número válido! Tente novamente:");
                     scan.nextLine();
                 }
-                num2 = scan.nextInt();
+                num2 = scan.nextDouble();
                 scan.nextLine();
                 result = num1 - num2;
                 System.out.println("O resultado da subtração é: " + result);
                 break;
             case "x":
                 System.out.println("Digite o número pelo qual você quer multiplicar o primeiro:");
-                while (!scan.hasNextInt()) {
-                    System.out.println("Erro: Isso não é um número inteiro válido! Tente novamente:");
+                while (!scan.hasNextDouble()) {
+                    System.out.println("Erro: Isso não é um número válido! Tente novamente:");
                     scan.nextLine();
                 }
-                num2 = scan.nextInt();
+                num2 = scan.nextDouble();
                 scan.nextLine();
                 result = num1 * num2;
                 System.out.println("O resultado da multiplicação é: " + result);
                 break;
             case "/":
                 System.out.println("Digite o número pelo qual você quer dividir o primeiro:");
-                while (!scan.hasNextInt()) {
-                    System.out.println("Erro: Isso não é um número inteiro válido! Tente novamente:");
+                while (!scan.hasNextDouble()) {
+                    System.out.println("Erro: Isso não é um número válido! Tente novamente:");
                     scan.nextLine();
                 }
-                num2 = scan.nextInt();
+                num2 = scan.nextDouble();
                 scan.nextLine();
-                result = num1 / num2;
-                System.out.println("O resultado da divisão é: " + result);
-                break;
+                if (num2 == 0) {
+                    System.out.println("Não é possível dividir por zero!");
+                    break;
+                }
+                else{
+                    result = num1 / num2;
+                    System.out.println("O resultado da divisão é: " + result);
+                    break;
+                }
 
         }
         System.out.println("Você quer fazer outra operação? (S, N)");
